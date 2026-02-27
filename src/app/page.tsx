4fb2cd4ae9f968a4719c8145ec74d0ef5a5457f3
tsedeniyafiseha <tsedeniyafisehaw@gmail.com>
2026-02-27 13:37:37 -0800

    Fix TypeScript error in page.tsx

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -11,7 +11,7 @@ import { reviewsData } from "@/data/reviews";
 export const dynamic = 'force-dynamic';
 
 export default async function Home() {
-  let allProducts = [];
+  let allProducts: any[] = [];
   let hasError = false;
   
   try {
